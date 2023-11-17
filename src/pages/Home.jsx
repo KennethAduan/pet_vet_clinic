9030ce2e9d8927fab7f922ebd217b7ea08e55ca3
TABRILLA <angelestabrilla10@gmail.com>
2023-11-17 22:30:53 +0800

    i

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,23 +48,6 @@ const HeroContent = () => {
   );
 };
 
-// function StarIcon() {
-//   return (
-//     <svg
-//       xmlns="http://www.w3.org/2000/svg"
-//       viewBox="0 0 24 24"
-//       fill="currentColor"
-//       className="w-5 h-5 text-yellow-700"
-//     >
-//       <path
-//         fillRule="evenodd"
-//         d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-//         clipRule="evenodd"
-//       />
-//     </svg>
-//   );
-// }
-// eslint-disable-next-line react/prop-types
 const TestimonialCard = ({ text, imageSrc, name }) => (
   <div className="p-8 transition-transform transform bg-white border rounded-lg hover:shadow-lg hover:scale-105">
     <p className="leading-loose text-gray-500 dark:text-gray-400">{text}</p>
@@ -76,14 +59,8 @@ const TestimonialCard = ({ text, imageSrc, name }) => (
       />
       <div className="mx-2">
         <h1 className="font-semibold text-gray-800 dark:text-white">{name}</h1>
-        <Rating value={5} readonly />
-        {/* <div className="flex items-center gap-0 5">
-          <StarIcon />
-          <StarIcon />
-          <StarIcon />
-          <StarIcon />
-          <StarIcon />
-        </div> */}
+          <Rating value={5} readonly />
+        
       </div>
     </div>
   </div>
@@ -193,16 +170,20 @@ const SendFeedBack = () => {
         <div className="flex justify-center mt-6">
           <div className="p-4 py-6 rounded-lg bg-gray-50 md:p-8 w-[600px]">
             <form className="space-y-4">
-              <div>
+              
+                {/* STAR */}
+                <Rating value={0} interactive="true" onChange={(value) => console.log(value)} />
+              
                 {/* Fullname */}
                 <Input label="Name" />
-              </div>
-
-              <Textarea label="Message" />
-
-              <Button fullWidth className="bg-FontMainColor">
-                Send Feedback
-              </Button>
+              
+                {/* MESSAGE */}
+                <Textarea label="Message" />
+              
+                <Button fullWidth className="bg-FontMainColor">
+                  Send Feedback
+                </Button>
+              
             </form>
           </div>
         </div>
